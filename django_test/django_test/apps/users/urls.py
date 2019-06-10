a0c3333dadfc5f6cd4fd0ccfbff5f32e9d9dc15f
jialebin <jlb@163.com>
2019-06-10 15:11:17 +0800

    添加查询用户名和手机号个数接口

--- django_test/django_test/apps/users/urls.py
+++ django_test/django_test/apps/users/urls.py
@@ -1,8 +1,11 @@
 from . import views
-from django.urls import path
+from django.urls import path, re_path
 
 
 urlpatterns = [
     path('hello/', views.hello),
-    path('create/', views.CreateUser.as_view())
+    path('create/', views.CreateUser.as_view()),
+    re_path(r'^count/username/(?P<username>\w+)/$', views.CountUserName.as_view()),
+    re_path(r'^count/mobile/(?P<mobile>1[3-9]\d{9})/$', views.CountUserMobile.as_view()),
 ]
+print(urlpatterns)
--- django_test/django_test/apps/users/views.py
+++ django_test/django_test/apps/users/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
 
 from .serializers import CreateUserSerializer
+from .models import User
 
 
 def hello(request):
@@ -13,5 +15,29 @@ def hello(request):
 
 
 class CreateUser(CreateAPIView):
-
+    """
+    注册用户
+    """
     serializer_class = CreateUserSerializer
+
+
+class CountUserName(APIView):
+    """
+    查询username数量
+    """
+    def get(self, request, username=None):
+        count = User.objects.filter(username=username).count()
+        return Response({'count': count})
+        pass
+    pass
+
+
+class CountUserMobile(APIView):
+    """
+    查询注册手机数量
+    """
+    def get(self, request, mobile=None):
+        count = User.objects.filter(mobile=mobile).count()
+        return Response({'count': count})
+        pass
+    pass
