caf7205fbb44b2c6d552fe39c7c4e20229d4e30e
jialebin <jlb@163.com>
2019-06-11 14:19:22 +0800

    注册时添加验证图片验证码

--- django_test/django_test/apps/users/serializers.py
+++ django_test/django_test/apps/users/serializers.py
@@ -1,3 +1,4 @@
+from django_redis import get_redis_connection
 from rest_framework import serializers
 import re
 from rest_framework_jwt.settings import api_settings
@@ -10,10 +11,13 @@ class CreateUserSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(label='确认密码', write_only=True)
     allow = serializers.BooleanField(label="同意协议", write_only=True)
     token = serializers.CharField(label='token', read_only=True)
+    image_code_text = serializers.CharField(label='图片验证码value', read_only=True)
+    image_code_id = serializers.CharField(label='图片验证码key', read_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'mobile', 'email', 'password2', 'allow', 'token')
+        fields = ('username', 'password', 'mobile', 'email', 'password2', 'allow', 'token', 'image_code_text',
+                  'image_code_id')
 
     def validate_allow(self, value):
         if value != True:
@@ -29,8 +33,21 @@ class CreateUserSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         password = attrs.get('password')
         password2 = attrs.get('password2')
+        image_code_text = attrs.get('image_code_text')
+        image_code_id = attrs.get('image_code_id')
+
         if password != password2:
             raise serializers.ValidationError('两次密码不一致')
+        redis_conn = get_redis_connection("verify_codes")
+        try:
+            image_server_code = redis_conn.get('img_%s' % image_code_id)
+        except Exception as e:
+            raise serializers.ValidationError('请重试')
+        if image_server_code is None:
+            raise serializers.ValidationError('验证码过期')
+        if image_server_code.decode() != image_code_text.upper():
+            raise serializers.ValidationError('验证码错误')
+
         return attrs
 
     def create(self, validated_data):
